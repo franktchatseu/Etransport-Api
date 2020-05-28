3581072cd6c43f0367f7492ddf85cf86d6f78055
richie <richienebou@gmail.com>
2020-05-28 12:54:36 +0100

    @user_input

---
+++ database/migrations/2020_05_27_172744_user_input_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserInputTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_inputs', function (Blueprint $table) {
+                $table->unsignedInteger('user_id');
+                $table->unsignedInteger('input_id');
+                $table->unsignedInteger('transaction_id');
+                $table->unsignedInteger('amount');
+                $table->date('date');
+                $table->string('city');
+                $table->string('provenance');
+                $table->string('country');
+                $table->string('pseudo');
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('input_id')->references('id')->on('inputs');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_inputs', function (Blueprint $table) {
+            //
+        });
+    }
+}
