22eaad32e857d8fed952f92beb2789d975a40d9c
arleon zemtsop <arleonzemtsop@outlook.fr>
2020-05-28 06:10:36 +0100

    Create model Nature in Finance Module V1

--- database/factories/NatureFactoriy.php
+++ database/factories/NatureFactoriy.php
@@ -2,11 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
 use App\Models\Finance\Nature;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Nature::class, function (Faker $faker) {
     return [
         'name' =>  $faker->text(20),
         'description' => $faker->text(100),
