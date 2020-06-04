a4aed0e39c0547ac6a656e8a98c0a4c4c347a196
richie <richienebou@gmail.com>
2020-06-04 08:23:50 +0100

    refresh@work

--- app/Http/Controllers/Catechesis/MemberController.php
+++ app/Http/Controllers/Catechesis/MemberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Catechesis;
 use App\Http\Controllers\Controller;
 use App\Models\Catechesis\Member;
 use Illuminate\Http\Request;
+use App\APIError;
 
 class MemberController extends Controller
 {
