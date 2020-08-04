66593b71d9092144dc1c9d5561c51498ff17aea5
adamshan <adamualiyu199@gmail.com>
2020-08-04 18:57:08 +0100

    WIP

--- app/Models/Module1/Stepper_Main.php
+++ app/Models/Module1/Stepper_Main.php
@@ -3,10 +3,11 @@
 namespace App\Models\Module1;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Stepper_Main extends Model
 {
-    //
+    use SoftDeletes;
     protected $table = 'stepper_mains';
 
 }
--- app/Models/Module3/steppertree.php
+++ app/Models/Module3/steppertree.php
@@ -3,9 +3,10 @@
 namespace App\Models\Module3;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class steppertree extends Model
 {
-    
+    use SoftDeletes;
     protected $table = 'stepper_trees';
 }
